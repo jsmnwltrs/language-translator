f95e3e22cabcdd38a4f18f77d6d2a4fc1a419515
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-08 10:25:41 -0500

    modified function

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+const german = {
+    "merry": "frohe", 
+    "christmas":"weihnachten", 
+    "and":"und", 
+    "happy":"glückliches", 
+    "new":"neues", 
+    "year":"jahr"
+};
 
+const spanish = {
+    "merry": "feliz", 
+    "christmas":"navidad", 
+    "and":"y", 
+    "happy":"próspero", 
+    "new":"nuevo", 
+    "year":"año"
+};
 
-
-const german = {"merry": "frohe", "christmas":"weihnachten", "and":"und", "happy":"glückliches", "new":"neues", "year":"jahr"};
-
-const spanish = {"merry": "feliz", "christmas":"navidad", "and":"y", "happy":"próspero", "new":"nuevo", "year":"año"};
-
-const french = {"merry": "joyeux", "christmas":"noel", "and":"et", "happy":"bonne", "new":"nouveau", "year":"année"};
+const french = {
+    "merry": "joyeux", 
+    "christmas":"noel", 
+    "and":"et", 
+    "happy":"bonne", 
+    "new":"nouveau", 
+    "year":"année"
+};
 
 
 const printToDom = (stringToPrint, divId) => {
@@ -14,29 +32,12 @@ const printToDom = (stringToPrint, divId) => {
 };
 
 let outputString = '';
-const translatorLoop = () => {
+const translatorLoop = (language) => {
 let inputText = document.getElementById('textarea').value.toLowerCase();
 let inputArray = inputText.split(' ');
 for (i = 0; i < inputArray.length; i++) {
-    outputString += `${german[inputArray[i]]}`;
+    outputString += `${language[inputArray[i]]}`;
     outputString += " ";
 };
 printToDom(outputString,'emptyDiv');
 };
-
-
-// const translatorLoop = () => {
-//     let stringBuilder = '';
-//     for (i = 0; i < inputArray.length; i++) {
-//         // stringBuilder += german.inputArray[i];
-
-//         // for (y = 0; y < german.length; y++) {
-//         //     let dicWord = german[y];
-//         //     if (textWord === dicWord){ 
-//         //         stringBuilder += dicWord;
-//         //     }   
-//         // }
-//     }
-//     printToDom(stringBuilder,'emptyDiv');
-//     return(stringBuilder)
-// };
