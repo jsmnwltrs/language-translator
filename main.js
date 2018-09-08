4f183350a88fd67844382dd2f86162acd8bb09c2
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-08 09:15:24 -0500

    figured out function

--- main.js
+++ main.js
@@ -6,3 +6,37 @@ const german = {"merry": "frohe", "christmas":"weihnachten", "and":"und", "happy
 const spanish = {"merry": "feliz", "christmas":"navidad", "and":"y", "happy":"próspero", "new":"nuevo", "year":"año"};
 
 const french = {"merry": "joyeux", "christmas":"noel", "and":"et", "happy":"bonne", "new":"nouveau", "year":"année"};
+
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+};
+
+let outputString = '';
+const translatorLoop = () => {
+let inputText = document.getElementById('textarea').value.toLowerCase();
+let inputArray = inputText.split(' ');
+for (i = 0; i < inputArray.length; i++) {
+    outputString += `${german[inputArray[i]]}`;
+    outputString += " ";
+};
+printToDom(outputString,'emptyDiv');
+};
+
+
+// const translatorLoop = () => {
+//     let stringBuilder = '';
+//     for (i = 0; i < inputArray.length; i++) {
+//         // stringBuilder += german.inputArray[i];
+
+//         // for (y = 0; y < german.length; y++) {
+//         //     let dicWord = german[y];
+//         //     if (textWord === dicWord){ 
+//         //         stringBuilder += dicWord;
+//         //     }   
+//         // }
+//     }
+//     printToDom(stringBuilder,'emptyDiv');
+//     return(stringBuilder)
+// };
